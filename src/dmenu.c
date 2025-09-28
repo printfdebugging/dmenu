02e845208f1d1dba2eb8caa828962ce5836b0d48
Sahil Gautam <printfdebugging@gmail.com>
2025-09-28 20:51:04 +0530

    no more floating dmenu

--- src/dmenu.c
+++ src/dmenu.c
@@ -66,16 +66,16 @@ static Colormap cmap;
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar               = 1;    /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar               = 0;    /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xff; /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 
-static int centered                  = 1;    /* -c option; centers dmenu on screen */
+static int centered                  = 0;    /* -c option; centers dmenu on screen */
 static int min_width                 = 1400; /* minimum width when centered */
 static const float menu_height_ratio = 2.0f; /* This is the ratio used in the original calculation */
 
-static unsigned int border_width = 2;
-static const char *fonts[]       = {"Iosevka Nerd Font:size=20"};
+static unsigned int border_width = 0;
+static const char *fonts[]       = {"Iosevka Nerd Font:size=14"};
 static const char *prompt        = NULL; /* -p  option; prompt to the left of input field */
 
 static const char *colors[SchemeLast][2] = {
